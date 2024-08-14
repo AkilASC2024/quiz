40e4e4a7d77b3d0060c35b87d7e2e293bd19a0e8
AkilASC2024 <akilkamara5@gmail.com>
2024-08-14 14:35:36 -0400

    demo day wooo

---
+++ data.js
@@ -0,0 +1,41 @@
+let questionslist = [
+    {
+        "thequestion": "1. did he diddy?",
+        "answers": [
+            "yes", 
+            "yes", 
+            "yes", 
+            "yes"
+        ],
+        "correct": 3
+    },
+    {
+        "thequestion": "joe biden free parking",
+        "answers": [
+            "yes",
+            "yes",
+            "yes",
+            "yes"
+        ],
+        "correct": 2
+    }
+]
+
+
+/*
+{
+    questions: [
+        {
+            "question1": {
+                "question": "did he diddy?",
+                "answers": {
+                    "choiceA": "yes",
+                    "choiceB": "yes",
+                    "choiceC": "yes",
+                    "choiceD": "yes"
+                }
+            }
+        }
+    ]
+};
+*/
